bccd70e4ca89e4b6066bf27c0d5d24a68748ad0f
Luis <lu.ca.oliveira41@gmail.com>
2021-01-05 21:38:35 -0400

    lambda functions

---
+++ handler.js
@@ -0,0 +1,80 @@
+'use strict';
+const { get } = require('axios')
+class Handler {
+  constructor({ rekoSvc, translatorSvc }){
+    this.rekoSvc = rekoSvc
+    this.translatorSvc = translatorSvc
+  }
+  async getImageBuffer(imageUrl){
+    const response = await get(imageUrl,{
+      responseType: 'arraybuffer'
+    })
+    const buffer = Buffer.from(response.data, 'base64')
+    return buffer
+  }
+  async detectImageLabels(buffer){
+    const result = await this.rekoSvc.detectLabels({
+      Image:{
+        Bytes: buffer
+      }
+    }).promise()
+    const workingItens = result.Labels.filter(({Confidence})=>Confidence>80)
+    const names = workingItens.map(({Name})=>Name).join(' e ')
+    return { names, workingItens }
+  }
+  async translateText(text){
+    const params = {
+      SourceLanguageCode: 'en',
+      TargetLanguageCode: 'pt',
+      Text: text
+    }
+    const res = await this.translatorSvc.translateText(params).promise()
+    return res.TranslatedText
+  }
+  formatTextResults(texts, workingItens){
+    const finalText = []
+    for(const indexText in texts){
+      const nameInPortuguese = texts[indexText]
+      const confidence = workingItens[indexText].Confidence
+      finalText.push(
+        `${confidence.toFixed(2)}% de ser do tipo ${nameInPortuguese}`
+      )
+    }
+    return finalText.join('\n')
+  }
+  async main(event){
+    try{
+      const { imageUrl } = event.queryStringParameters
+      if(!imageUrl){
+        return {
+          statusCode: 403,
+          body: 'Invalid imageUrl'
+        }
+      }
+      const imgBuffer = await this.getImageBuffer(imageUrl)
+      const { names, workingItens } = await this.detectImageLabels(imgBuffer)
+      const texts = await this.translateText(names)
+      const finalText = this.formatTextResults(texts.split(' e '), workingItens)      
+
+      return {
+        statusCode: 200,
+        body: `A imagem tem\n `.concat(finalText)
+      }
+    }catch(err){
+      return {
+        statusCode: 500,
+        body: `Internal Server Error `
+      }
+    }
+  }
+}
+
+const aws = require('aws-sdk')
+const reko = new aws.Rekognition()
+const translator = new aws.Translate()
+const handler = new Handler({
+  rekoSvc: reko,
+  translatorSvc: translator
+})
+
+module.exports.main = handler.main.bind(handler)
